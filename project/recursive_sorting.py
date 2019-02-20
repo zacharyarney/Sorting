5687e0c928cd6245975cd2bdf38aefa62f0b6aa4
zacharyarney <zachary.arney@gmail.com>
2019-02-20 00:00:43 -0800

    Formatting

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -1,11 +1,12 @@
-### helper function
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# helper function
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     a = 0
     b = 0
-    # since arrA and arrB already sorted, we only need to compare the first element of each when merging!
-    for i in range( 0, elements ):
+    # since arrA and arrB already sorted, we only need to compare the first
+    # element of each when merging!
+    for i in range(0, elements):
         if a >= len(arrA):    # all elements in arrA have been merged
             merged_arr[i] = arrB[b]
             b += 1
@@ -21,12 +22,12 @@ def merge( arrA, arrB ):
     return merged_arr
 
 
-### recursive sorting function
-def merge_sort( arr ):
-    if len( arr ) > 1:
-        left = merge_sort( arr[ 0 : len( arr ) / 2 ] )
-        right = merge_sort( arr[ len( arr ) / 2 : ] )
-        arr = merge( left, right )   # merge() defined later
+# recursive sorting function
+def merge_sort(arr):
+    if len(arr) > 1:
+        left = merge_sort(arr[0: len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2:])
+        arr = merge(left, right)   # merge() defined later
     return arr
 
 
